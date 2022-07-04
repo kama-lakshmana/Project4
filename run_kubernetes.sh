4fa6d72307bc1b5002bea759405348163e82a0a0
Kama Lakshmana <kama.lakshmana@gmail.com>
2022-07-04 22:35:54 +0530

    Project4

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=kamalakshmana/udacity
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udacitypod --image=$dockerpath --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods -A
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacitypod 8000:80
+
